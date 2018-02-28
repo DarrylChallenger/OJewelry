c51b54ee87da91b837516e448e51e25a8e289c7a
Darryl Challenger <darryl.challenger@outlook.com>
2018-02-28 11:19:20 -0500

    Correct issue selecting the correct fields on component page (correction in js)

--- OJewelry/Scripts/CreateEditComponents.js
+++ OJewelry/Scripts/CreateEditComponents.js
@@ -8,48 +8,45 @@ $(function () {
 
 function resetControls() {
     console.log("resetControls");
-    var sel = document.getElementById("ComponentTypeId").selectedIndex;
+    var sel = $("#ComponentTypeId option:selected").text();
     var ctl;
-    switch (sel) {
-        case 1: // Stones
-            document.getElementById("ecVendorGroup").style.display = "block";
-            document.getElementById("ecPriceGroup").style.display = "none";
-            document.getElementById("ecPPHGroup").style.display = "none";
-            document.getElementById("ecPPPGroup").style.display = "none";
-            document.getElementById("ecMetalMetalGroup").style.display = "none";
-            document.getElementById("ecMetalLaborGroup").style.display = "none";
-            document.getElementById("ecCtwtGroup").style.display = "block";
-            document.getElementById("ecStoneSizeGroup").style.display = "block";
-            document.getElementById("ecStonePPCGroup").style.display = "block";
-            document.getElementById("ecFindingsMetalGroup").style.display = "none";
-           break;
-        case 2: // Findings
-            document.getElementById("ecVendorGroup").style.display = "block";
-            document.getElementById("ecPriceGroup").style.display = "block";
-            document.getElementById("ecPPHGroup").style.display = "none";
-            document.getElementById("ecPPPGroup").style.display = "none";
-            document.getElementById("ecMetalMetalGroup").style.display = "block";
-            document.getElementById("ecMetalLaborGroup").style.display = "none";
-            document.getElementById("ecCtwtGroup").style.display = "none";
-            document.getElementById("ecStoneSizeGroup").style.display = "none";
-            document.getElementById("ecStonePPCGroup").style.display = "none";
-            document.getElementById("ecFindingsMetalGroup").style.display = "none";
-            break;
-        default:
-            console.log("Illegal componentent type: " + sel);
-            document.getElementById("ecVendorGroup").style.display = "none";
-            document.getElementById("ecPriceGroup").style.display = "none";
-            document.getElementById("ecPPHGroup").style.display = "none";
-            document.getElementById("ecPPPGroup").style.display = "none";
-            document.getElementById("ecMetalMetalGroup").style.display = "none";
-            document.getElementById("ecMetalLaborGroup").style.display = "none";
-            document.getElementById("ecCtwtGroup").style.display = "none";
-            document.getElementById("ecStoneSizeGroup").style.display = "none";
-            document.getElementById("ecStonePPCGroup").style.display = "none";
-            document.getElementById("ecFindingsMetalGroup").style.display = "none";
-            break;
-}
-    console.log(sel);
+    if (sel === "Stones") {
+        document.getElementById("ecVendorGroup").style.display = "block";
+        document.getElementById("ecPriceGroup").style.display = "none";
+        document.getElementById("ecPPHGroup").style.display = "none";
+        document.getElementById("ecPPPGroup").style.display = "none";
+        document.getElementById("ecMetalMetalGroup").style.display = "none";
+        document.getElementById("ecMetalLaborGroup").style.display = "none";
+        document.getElementById("ecCtwtGroup").style.display = "block";
+        document.getElementById("ecStoneSizeGroup").style.display = "block";
+        document.getElementById("ecStonePPCGroup").style.display = "block";
+        document.getElementById("ecFindingsMetalGroup").style.display = "none";
+    }
+    if (sel === "Findings") {
+        document.getElementById("ecVendorGroup").style.display = "block";
+        document.getElementById("ecPriceGroup").style.display = "block";
+        document.getElementById("ecPPHGroup").style.display = "none";
+        document.getElementById("ecPPPGroup").style.display = "none";
+        document.getElementById("ecMetalMetalGroup").style.display = "block";
+        document.getElementById("ecMetalLaborGroup").style.display = "none";
+        document.getElementById("ecCtwtGroup").style.display = "none";
+        document.getElementById("ecStoneSizeGroup").style.display = "none";
+        document.getElementById("ecStonePPCGroup").style.display = "none";
+        document.getElementById("ecFindingsMetalGroup").style.display = "none";
+    }
+    if (sel !== "Stones" && sel !== "Findings") {
+        console.log("Illegal componentent type: " + sel);
+        document.getElementById("ecVendorGroup").style.display = "none";
+        document.getElementById("ecPriceGroup").style.display = "none";
+        document.getElementById("ecPPHGroup").style.display = "none";
+        document.getElementById("ecPPPGroup").style.display = "none";
+        document.getElementById("ecMetalMetalGroup").style.display = "none";
+        document.getElementById("ecMetalLaborGroup").style.display = "none";
+        document.getElementById("ecCtwtGroup").style.display = "none";
+        document.getElementById("ecStoneSizeGroup").style.display = "none";
+        document.getElementById("ecStonePPCGroup").style.display = "none";
+        document.getElementById("ecFindingsMetalGroup").style.display = "none";
+    }
 }
 
 function showControl() {
