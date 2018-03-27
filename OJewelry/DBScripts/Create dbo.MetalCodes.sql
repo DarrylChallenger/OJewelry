3e908c078034c9bc5df6d88b04a97c30f391b8f4
Darryl Challenger <darryl.challenger@outlook.com>
2018-03-27 10:00:18 -0400

    Add Security

---
+++ OJewelry/DBScripts/Create dbo.MetalCodes.sql
@@ -0,0 +1,17 @@
+﻿USE [OJewelryDB]
+GO
+
+/****** Object: Table [dbo].[MetalCodes] Script Date: 3/26/2018 9:39:35 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[MetalCodes] (
+    [Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [Code] NCHAR (6)    NULL,
+    [Desc] VARCHAR (50) NULL
+);
+
+
