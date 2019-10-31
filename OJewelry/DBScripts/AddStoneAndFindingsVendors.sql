d27a27fda2e0cebca3d6370b51d24b18371a5de8
Darryl Challenger <darryl.challenger@outlook.com>
2019-10-30 22:28:15 -0400

    Update db script to set stones/findings with appropriate vendor

--- OJewelry/DBScripts/AddStoneAndFindingsVendors.sql
+++ OJewelry/DBScripts/AddStoneAndFindingsVendors.sql
@@ -19,7 +19,39 @@ FROM Companies as c
 WHERE NOT EXISTS (SELECT * from Vendors as v where v.CompanyId = c.id and v.Type_Type = 3)  
 commit transaction
 
+/* Check */
 select * from Vendors
 select c.Id, c.Name, v.* from Companies c
 left  join Vendors v on v.CompanyId = c.id
 
+select * from Vendors where Type_Type <> 0
+
+/* Check */
+select * from stones s
+join vendors v on v.CompanyId = s.CompanyId 
+where vendorId is null and v.Type_Type = 3
+select * from findings f
+join vendors v on v.CompanyId = f.CompanyId 
+where vendorId is null and v.Type_Type = 5
+
+/* Assign stone vendor to stones */
+begin tran
+update stones set vendorId = vid from
+(
+select v.id as vid, s.id as sid from stones s
+join vendors v on v.CompanyId = s.CompanyId 
+where vendorId is null and v.Type_Type = 3
+) t where id = sid
+select * from stones 
+commit tran
+
+/* Assign findings vendor to findings */
+begin tran
+update findings set vendorId = vid from
+(
+select v.id as vid, f.id as fid from findings f
+join vendors v on v.CompanyId = f.CompanyId 
+where vendorId is null and v.Type_Type = 5
+) t where id = fid
+select * from findings 
+commit tran
